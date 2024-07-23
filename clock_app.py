b81e712611cba0e5f11b48147b061299ab8b8138
Sam Johnson <80408211+SamJ8@users.noreply.github.com>
2024-07-23 15:44:27 +0100

    first commit

---
+++ clock_app.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%X %p")
+    time_label.config(text=time_string)
+
+    time_label.after(1000, update)
+
+    day_string = strftime("%a, %d %b %Y", gmtime())
+    day_label.config(text=day_string)
+
+    day_label.after(1000, update)
+
+window = Tk()
+
+time_label = Label(window, font=("Courier New",55, "bold"), fg="#FFF813", bg="black")
+time_label.pack()
+
+day_label = Label(window, font=("Courier New",55, "bold"), fg="#FFF813", bg="black")
+day_label.pack()
+
+update()
+
+window.mainloop()
